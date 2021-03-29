17b0c18019ad247989c1b17a650115f4f2086a0d
arbid2544 <mrarbid.m@ku.th>
2021-03-29 19:04:35 +0700

    Update Nearest Number.cpp

--- Nearest Number.cpp
+++ Nearest Number.cpp
@@ -1,8 +1,24 @@
 #include<stdio.h>
 int main()
 {
-	int n,c,i;
+    int n,c,i,k;
 	scanf("%d %d",&n,&c);
+    /*if(c<10&&c>0)
+    {
+    	k=10;
+	}
+	else if(c<100&&c>=10)
+	{
+		k=100;
+	}
+	else if(c<1000&&c>=100)
+	{
+		k=1000;
+	}
+	else if(c<10000&&c>=1000)
+	{
+		k=10000;
+	}*/
 	for(i=n;i<n+10;i++)
 	{
 		if(i%10==c)
