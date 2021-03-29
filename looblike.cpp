bfa66c3c8069fd94df80ce1ed27aecd02cfebe0f
arbid2544 <mrarbid.m@ku.th>
2021-03-29 18:11:47 +0700

    Update looblike.cpp

--- looblike.cpp
+++ looblike.cpp
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,max=0,Max=0,F=0;
+	int n,i,max=0,Max=0,F=0,X;
 	scanf("%d",&n);
-	int A[n],B[10000]={0};
+	int A[n],B[10000]={0},C[10000]={0};
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&A[i]);
@@ -23,25 +23,30 @@ int main()
 		//	printf("%d ",B[i]);
 		}
 	}
-	for(i=1;i<=n;i++)
+/*	for(i=1;i<=n+max;i++)
 	{
-		if(B[i]>Max)
-		{
-			Max=A[i];
-		}
-	}
-/*	for(i=1;i<=max;i++)
-	{
-		printf("%d-%d\n",i,B[i]);
-	}
-	for(i=0;i<n;i++)
-	{
-		if(B[i]>Max)
-		{
-			Max=B[i];
-			F=i;
-		}
+			printf("%d ",B[i]);
 	}*/
-	printf("%d",Max);
+	for(i=1;i<=n+max;i++)
+	{
+		if(B[i]>=Max)
+		{
+			Max=i;
+			X=B[i];
+		//	printf(",%d ",Max);
+		}
+	}
+	for(i=1;i<=n+max;i++)
+	{
+		if(B[i]==X)
+		{
+			C[i]=i;
+		}
+	}
+    for(i=1;i<=n+max;i++)
+	{
+		if(C[i]!=0)
+		printf("%d ",C[i]);
+	}  
 	return 0;
 }
