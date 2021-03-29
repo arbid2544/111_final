529adc4fb1e7d6ff9ec4c6f7a94b1525c7dd6ad5
arbid2544 <mrarbid.m@ku.th>
2021-03-29 19:19:33 +0700

    Create No Magic Square.cpp

---
+++ No Magic Square.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,a,b,c;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	int X[a][a],Y[b][b],Z[c][c];
+/*	for(i=0;i<a;i++)
+	   for(j=0;j<a;j++)
+	      scanf("%d",&X[a][a]);
+	for(i=0;i<b;i++)
+	   for(j=0;j<b;j++)
+	      scanf("%d",&Y[b][b]);*/
+	for(i=0;i<c;i++)
+	   for(j=0;j<c;j++)
+	      scanf("%d",&Z[c][c]);
+	      
+	return 0;
+}
